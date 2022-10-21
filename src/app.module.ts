1b4bcb66f3518c7fca024309dae6624f83b0548e
ad105geppetto <ad105geppetto@gmail.com>
2022-10-21 23:57:28 +0900

    feat: 유저 entity 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { UsersModule } from "./users/users.module";
+import { User } from "./users/entity/user.entity";
 
 @Module({
   imports: [
@@ -15,7 +16,7 @@ import { UsersModule } from "./users/users.module";
       username: "root",
       password: process.env.MYSQL_PASSWORD,
       database: process.env.MYSQL_NAME,
-      entities: [],
+      entities: [User],
       synchronize: true,
     }),
     UsersModule,
