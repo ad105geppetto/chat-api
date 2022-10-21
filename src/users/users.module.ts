253c0202cf90f2e122819809a71c003d734c2b0f
ad105geppetto <ad105geppetto@gmail.com>
2022-10-22 01:53:58 +0900

    feat: 유저 레포지토리 추가

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { UsersController } from "./controllers/users.controller";
 import { UsersService } from "./services/users.service";
 import { User } from "./entity/user.entity";
+import { UsersRepository } from "./repositories/users.repository";
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, UsersRepository],
 })
 export class UsersModule {}
