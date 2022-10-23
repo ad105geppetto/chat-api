2cec5e29faae8653d0d04a3405bc33486d269e7a
ad105geppetto <ad105geppetto@gmail.com>
2022-10-24 05:10:34 +0900

    feat: 유저 레포지토리 수정

--- src/users/repositories/users.repository.ts
+++ src/users/repositories/users.repository.ts
@@ -1,4 +1,20 @@
 import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { DataSource } from "typeorm";
+import { User } from "../entity/user.entity";
 
 @Injectable()
-export class UsersRepository {}
+export class UsersRepository {
+  constructor(
+    @InjectRepository(User)
+    private readonly dataSource: DataSource,
+  ) {}
+
+  async create(email: string) {
+    const user = new User();
+
+    user.email = email;
+
+    await this.dataSource.manager.save(user);
+  }
+}
