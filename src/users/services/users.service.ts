75ad44f0662bf537b4661c6fc9e3465cf13e5b88
ad105geppetto <ad105geppetto@gmail.com>
2022-10-24 05:05:29 +0900

    feat: 유저 서비스 수정
    # 내용
    - 유저의 권한인증 코드를 구글 서버에 보내어 토큰 정보를 불러옵니다.
    - 받은 토큰 정보를 구글 서버에 다시 보내어 유저의 정보를 받습니다.
    - 유저의 정보 중 E-mail만 레포지토리로 보냅니다.
    - 이메일과 Access Token을 반환합니다.

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,4 +1,44 @@
+import { HttpService } from "@nestjs/axios";
 import { Injectable } from "@nestjs/common";
+import { lastValueFrom } from "rxjs";
+import { AuthCodeDTO } from "../dto/authCode.dto";
+import { UsersRepository } from "../repositories/users.repository";
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    private readonly httpService: HttpService,
+    private readonly usersRepository: UsersRepository,
+  ) {}
+
+  async login(authCode: AuthCodeDTO) {
+    const tokenInfo = await lastValueFrom(
+      this.httpService.post(`https://oauth2.googleapis.com/token`, {
+        code: authCode.authorizationCode,
+        client_id: process.env.GOOGLE_CLIENT_ID,
+        client_secret: process.env.GOOGLE_CLIENT_PASSWORD,
+        redirect_uri: "http://localhost:3000",
+        grant_type: "authorization_code",
+      }),
+    );
+
+    const accessToken = tokenInfo.data.access_token;
+
+    const userInfo = await lastValueFrom(
+      this.httpService.get(
+        `https://www.googleapis.com/oauth2/v2/userinfo?access_token=${accessToken}`,
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        },
+      ),
+    );
+
+    const userEmail = userInfo.data.email;
+
+    this.usersRepository.create(userEmail);
+
+    return { email: userEmail, accessToken };
+  }
+}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,9 +4,10 @@ import { UsersController } from "./controllers/users.controller";
 import { UsersService } from "./services/users.service";
 import { User } from "./entity/user.entity";
 import { UsersRepository } from "./repositories/users.repository";
+import { HttpModule } from "@nestjs/axios";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), HttpModule],
   controllers: [UsersController],
   providers: [UsersService, UsersRepository],
 })
